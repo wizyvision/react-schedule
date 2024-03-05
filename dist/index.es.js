2e3543a57280c1581d18858d8c3cedf1539440c3
Coeli <coeli@wizy.io>
2024-03-05 18:51:04 +0800

    + dist folder

---
+++ dist/index.es.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Scheduler = () => {
+  return /*#__PURE__*/React.createElement("div", null, "Hello world");
+};
+
+export { Scheduler };
---
+++ dist/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var React = require('react');
+
+function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
+
+var React__default = /*#__PURE__*/_interopDefaultLegacy(React);
+
+const Scheduler = () => {
+  return /*#__PURE__*/React__default["default"].createElement("div", null, "Hello world");
+};
+
+exports.Scheduler = Scheduler;
