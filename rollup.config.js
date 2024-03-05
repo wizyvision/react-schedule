6d627efb95340077baa284abb2f1dacdf5511506
Coeli <coeli@wizy.io>
2024-03-05 18:41:17 +0800

    + reinitialize project

---
+++ rollup.config.js
@@ -0,0 +1,33 @@
+
+const babel = require('rollup-plugin-babel') ;
+const resolve = require('@rollup/plugin-node-resolve')
+const external = require('rollup-plugin-peer-deps-external')
+const commonjs = require('@rollup/plugin-commonjs')
+
+// eslint-disable-next-line import/no-anonymous-default-export
+module.exports = [
+  {
+    input: './src/index.js',
+    output: [
+      {
+        file: 'dist/index.js',
+        format: 'cjs',
+      },
+      {
+        file: 'dist/index.es.js',
+        format: 'es',
+        exports: 'named',
+      },
+    ],
+    plugins: [
+        babel({
+            exclude: 'node_modules/**',
+            presets: ['@babel/preset-react'],
+        }),
+        external(),
+        resolve(),
+        commonjs()
+    ]
+  },
+  
+];
---
+++ src/stories/Scheduler.stories.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Scheduler } from '../lib/components/Scheduler'
+
+export default {
+  title: 'Scheduler',
+  component: Scheduler,
+  tags: ['autodocs'],
+};
+
+const Template = (args) => {
+
+  return (<Scheduler {...args} />);
+};
+
+export const ResourceTimeline = Template.bind({});
+
