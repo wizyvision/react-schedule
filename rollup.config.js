9a8277b06c4a626b96b968fd71f71755d223306b
Coeli <coeli@wizy.io>
2024-03-06 18:45:26 +0800

    * update dist

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,13 @@
-const babel = require('rollup-plugin-babel') ;
+import PeerDepsExternalPlugin from 'rollup-plugin-peer-deps-external';
+const babel = require('rollup-plugin-babel');
 const resolve = require('@rollup/plugin-node-resolve')
-const external = require('rollup-plugin-peer-deps-external')
 const commonjs = require('@rollup/plugin-commonjs')
 
 
 // eslint-disable-next-line import/no-anonymous-default-export
 module.exports = [
   {
-    input: './src/index.js',
+    input: 'src/index.js',
     output: [
       {
         file: 'dist/index.js',
@@ -24,7 +24,7 @@ module.exports = [
             exclude: 'node_modules/**',
             presets: ['@babel/preset-react'],
         }),
-        external(),
+        PeerDepsExternalPlugin(),
         resolve(),
         commonjs()
     ]
