f5dbf0f32e06f94920c48ccab594900f84b33e64
Coeli <coeli@wizy.io>
2024-03-08 16:37:01 +0800

    * fix loader issue

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import PeerDepsExternalPlugin from 'rollup-plugin-peer-deps-external';
+import terser from '@rollup/plugin-terser'
 const babel = require('rollup-plugin-babel');
 const resolve = require('@rollup/plugin-node-resolve')
 const commonjs = require('@rollup/plugin-commonjs')
@@ -26,7 +27,8 @@ module.exports = [
         }),
         PeerDepsExternalPlugin(),
         resolve(),
-        commonjs()
+        commonjs(),
+        terser()
     ]
   },
   
