71a9ffed4cc819de8fe9e492a3fe65af29d6eec4
Coeli <coeli@wizy.io>
2024-05-08 18:44:16 +0800

    applying theme in header

--- src/lib/components/Header/DatePicker/index.js
+++ src/lib/components/Header/DatePicker/index.js
@@ -5,6 +5,7 @@ import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import moment from 'moment';
 
 import { useSchedulerContext } from '../../../context/SchedulerProvider';
+import { useTheme } from '@mui/system';
 
 
 function ButtonField(props) {
@@ -30,6 +31,13 @@ function ButtonField(props) {
       disabled={disabled}
       ref={ref}
       aria-label={ariaLabel}
+      sx={{
+        color: 'primary.main', // Default to contrast text color from theme
+        textTransform: 'capitalize',
+        '&:hover': {
+          backgroundColor: 'primary.main.light', // Default to lighter primary color on hover
+        },
+      }}
     >
       <Typography
         sx={{
@@ -45,6 +53,8 @@ function ButtonField(props) {
 function SchedulerDatePicker(props) {
   const { date, onDateChange } = useSchedulerContext();
   const [open, setOpen] = useState(false);
+
+  const theme = useTheme()
 
   const handleOpen = () => {
     setOpen(true)
@@ -71,6 +81,15 @@ function SchedulerDatePicker(props) {
       }}
       slotProps={{
         field: { setOpen },
+      }}
+      sx={{
+        '& .MuiPickersDay-daySelected': {
+          backgroundColor: theme.palette.primary.main, // Use main color from theme.palette[color]
+          color: theme.palette.primary.contrastText, // Use contrast text color from theme.palette[color]
+        },
+        '& .MuiPickersDay-daySelected:hover': {
+          backgroundColor: theme.palette.primary.main.light, // Use light color from theme.palette[color] on hover
+        },
       }}
     />
   );
